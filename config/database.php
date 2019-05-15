63c1315cb53a19ce59d70a2d24a6bcfa5f8cef64
Viraj Zaveri <viraj.zaveri@diamonds.net>
2019-05-15 15:38:38 +0530

    Revert

--- config/database.php
+++ config/database.php
@@ -1,5 +1,4 @@
 <?php
-$DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 
 return [
 
@@ -62,15 +61,16 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
