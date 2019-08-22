e719976e8e9bbba228f3001de5c5bf4fdd21faaa
Viraj Zaveri <viraj.zaveri@diamonds.net>
2019-08-22 12:37:52 +0530

    Add Temp Auth

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -24,6 +24,10 @@ use App\Rough;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
