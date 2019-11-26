d32fa24ab105137419e7d10d0b8f0044d6a5f482
Viraj Zaveri <viraj.zaveri@diamonds.net>
2019-11-26 17:14:55 +0530

    Update Seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,8 +12,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\User')->create(['email' => 'viraj@d.net', 'name' => 'Viraj Zaveri']);
-        factory('App\User')->create(['email' => 'shimon@d.net', 'name' => 'Shimon Gerstensang']);
-        factory('App\User')->create(['email' => 'amit@d.net', 'name' => 'Amit Kronfeld']);
+        factory('App\User')->create(['email' => 'viraj@d.net', 'name' => 'Viraj Zaveri', 'password' => Hash::make('VZ@1234')]);
+        factory('App\User')->create(['email' => 'shimon@d.net', 'name' => 'Shimon Gerstensang', 'password' => Hash::make('SG@1234')]);
+        factory('App\User')->create(['email' => 'amit@d.net', 'name' => 'Amit Kronfeld', 'password' => Hash::make('AK@1234')]);
+        factory('App\User')->create(['email' => 'rap@d.net', 'name' => 'Martin Rapaport', 'password' => Hash::make('MR@1234')]);
     }
 }
